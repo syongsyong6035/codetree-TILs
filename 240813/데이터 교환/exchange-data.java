0d94404e62be2abeacc479e109980835c60713ab
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2024-08-13 16:27:08 +0900

    Title: 데이터 교환, Time: 88ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 240813/데이터 교환/exchange-data.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int a,b,c,d;
+        a=5;
+        b=6;
+        c=7;
+        d=a;
+        a=c;
+        c=b;
+        b=d;
+
+       
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
