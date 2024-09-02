cedc6ae7f09d864c97d5c44d3733deb015a8607c
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2024-09-02 18:45:45 +0900

    Title: 같은 종류끼리 모으기, Time: 150ms, Memory: 11MB, Status: Wrong Answer - CodeTree

--- 240902/같은 종류끼리 모으기/group-together.java
+++ 240902/같은 종류끼리 모으기/group-together.java
@@ -4,22 +4,40 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
 
-        int a = sc.nextInt();
+        double a = sc.nextDouble();
         sc.nextLine(); // 입력 버퍼에 남아있는 개행 문자를 제거
 
         String b = sc.nextLine(); 
-        int result;
-        int su = b.replace("a","").length();
-
-        if(a / 2 >= su){
-            if(!b.isEmpty() && b.charAt(0) == 'a'){  
-                result = su - 1;
+        double result;
+        double su = b.replace("a", "").length();
+        double num = 1;
+     
+        if (a / 2 > su) {
+            if (!b.isEmpty() && b.charAt(0) == 'a') {  
+             
+                for (int i = 0; i < su; i++) {
+                    if (b.charAt(i) == 'b') {
+                        break;
+                    }
+                    else if (b.charAt(i) == 'a') {
+                        ++num;
+                    }
+                }
+                result = su - num;
             } else {
                 result = su;
             }
         } else {
-            if(!b.isEmpty() && b.charAt(0) == 'a'){  
-                result = a - su - 1;
+            if (!b.isEmpty() && b.charAt(0) == 'b') {  
+                for (int i = 0; i < su; i++) {
+                    if (b.charAt(i) == 'a') {
+                        break;
+                    }
+                    else if (b.charAt(i) == 'b') {
+                        ++num;
+                    }
+                }
+                result = a - su - num;
             } else {
                 result = a - su;
             }
