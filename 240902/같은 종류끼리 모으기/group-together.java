9348c7f66e25ceefdb9fe1127110e3621ac448b4
syongsyong6035 <130562778+syongsyong6035@users.noreply.github.com>
2024-09-02 18:51:18 +0900

    Title: 같은 종류끼리 모으기, Time: 119ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240902/같은 종류끼리 모으기/group-together.java
+++ 240902/같은 종류끼리 모으기/group-together.java
@@ -1,24 +1,30 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        sc.nextLine(); 
+        sc.nextLine(); // Consume the newline character
         String s = sc.nextLine();
 
-        int blockCount = 1;
+        // Edge case: empty or single character string
+        if (n == 0) {
+            System.out.println(0);
+            return;
+        }
 
- 
+        // Initialize block count
+        int blockCount = 1; // Start with 1 block
+        
+        // Count the number of contiguous blocks
         for (int i = 1; i < n; i++) {
             if (s.charAt(i) != s.charAt(i - 1)) {
                 blockCount++;
             }
         }
 
-       
-        int minMoves = blockCount / 2;
-
-        System.out.println(minMoves);
+        // The minimum number of moves needed is the number of blocks minus 1
+        // This is because if you have k blocks, you need (k-1) moves to merge them all into one.
+        System.out.println((blockCount + 1) / 2);
     }
 }
